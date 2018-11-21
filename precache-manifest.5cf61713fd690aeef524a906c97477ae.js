6ac2dc07bd3e410875e11e754e44d3db58d179df
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-21 12:54:14 -0800

    Updates

---
+++ precache-manifest.5cf61713fd690aeef524a906c97477ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c3866b8cb487892b283",
+    "url": "/codechallenge/static/js/runtime~main.0c3866b8.js"
+  },
+  {
+    "revision": "1254e6315c4722f8e21b",
+    "url": "/codechallenge/static/js/main.1254e631.chunk.js"
+  },
+  {
+    "revision": "767d56d792a7e49fd909",
+    "url": "/codechallenge/static/js/1.767d56d7.chunk.js"
+  },
+  {
+    "revision": "1254e6315c4722f8e21b",
+    "url": "/codechallenge/static/css/main.e7caa919.chunk.css"
+  },
+  {
+    "revision": "767d56d792a7e49fd909",
+    "url": "/codechallenge/static/css/1.498b17d7.chunk.css"
+  },
+  {
+    "revision": "385a94da2aeebf758f91712410c028e5",
+    "url": "/codechallenge/index.html"
+  }
+];

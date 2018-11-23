028954ece4dfec9158507847f8a122dc973d07f7
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-23 12:00:41 -0800

    App top level render test

--- src/modules/App/App.test.js
+++ src/modules/App/App.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
+import App from './App';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  
+configure({ adapter: new Adapter() });
+
+describe("App", () =>{
+	it('Renders properly', () => {
+	  const app = shallow(<App/>);
+	  expect(app).toMatchSnapshot();
+	});
 });

3d4b96bd9880bf19c4617705fd0288e2367da724
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-23 13:02:17 -0800

    Set current course tests

--- src/modules/Courses/Course.test.js
+++ src/modules/Courses/Course.test.js
@@ -25,5 +25,54 @@ describe("Course", () =>{
 	  course.instance({ params: props.match.params});
 	  	expect(course).toMatchSnapshot();
 	});
+});
 
+
+describe("Set Current Course", () =>{
+	const props = {
+		"match":
+			{
+				"params": {
+					"courseid": 51,
+					"coursename": "English I"
+			}
+		}
+	}
+
+	 
+	it('adds the current course to student object', () => {
+		const course = shallow(<Course {...props}/>);
+		course.instance({ params: props.match.params});
+		console.log(course);
+		let student =   {
+	       "Classes":  [
+	          {
+	           "id": 93,
+	           "score": 92,
+	         },
+	          {
+	           "id": 51,
+	           "score": 12,
+	         },
+	          {
+	           "id": 148,
+	           "score": 93,
+	         },
+	          {
+	           "id": 138,
+	           "score": 80,
+	         },
+	       ],
+	       "email": "jflanagano@yellowpages.com",
+	       "first_name": "Jude",
+	       "gender": "Male",
+	       "id": 25,
+	       "last_name": "Flanagan",
+	     };
+		expect(("currentClass" in student)).toBe(false);
+		expect(("currentClassScore" in student)).toBe(false);
+		student = course.instance().setCurrentCourse(student);
+		expect(("currentClass" in student)).toBe(true);
+		expect(("currentClassScore" in student)).toBe(true);
+	});
 });

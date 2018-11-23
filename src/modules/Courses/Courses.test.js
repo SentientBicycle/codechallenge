0f7eeb8866bc3e850b51c07f237ba8dd075ce27f
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-23 11:17:58 -0800

    Courses test snapshot

--- src/modules/Courses/Courses.test.js
+++ src/modules/Courses/Courses.test.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Courses from './Courses'
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  
+configure({ adapter: new Adapter() });
+
+describe("Courses", () =>{
+	it('Renders properly', () => {
+	  const course = shallow(<Courses/>);
+	 // course.instance({ params: props.match.params});
+	  expect(course).toMatchSnapshot();
+	});
 });
